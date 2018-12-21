a74b33f443bd2d863d4fd309580e39de86991c26
SiwyKrzysiek <k.fajny@gmail.com>
2018-12-21 13:09:52 +0100

    Wyłapywanie nieprawidłowych argumentów

--- skrypt.sh
+++ skrypt.sh
@@ -3,7 +3,14 @@
 wd=$(pwd)
 # echo $wd
 
-extention=sh
+[[ 5 -gt 2 || 10 -gt 0 ]] && echo Tak
+
+if [[ -z $1 || -z $2 ]]; then
+    echo Nieprawidłowe argumenty
+fi
+
+extention=$1
+destynationCatalog=$2
 
 for file in ./*.$extention
 do
